bc779e5749fac353e12bc6aaff274ad20fffd11c
John Paul <paulloayon6406@gmail.com>
2025-03-06 20:33:52 -0800

    Initial commit

--- users/user.controller.js
+++ users/user.controller.js
@@ -29,3 +29,43 @@ function create(req, res, next) {
         .then(() => res.json({ message: 'User created' }))
         .catch(next);
 }
+
+function update(req,res,next){
+    userService.update(req.params.id, req.body)
+    .then(()=> res.json({message: 'User updated'}))
+    .catch(next);
+}
+
+function _delete(req, res, next){
+    userService.delete(req.params.id)
+    .then(()=> res.json({message: 'User deleted'}))
+    .catch(next);
+}
+
+function createSchema(req,res,next){
+    const schema = Joi.object({
+        title: Joi.string().required(),
+        firstName: Joi.string().required(),
+        lastName: Joi.string().required(),
+        role: Joi.string().valid(Role.Admin, Role.User).required(),
+        email: Joi.string().email().required(),
+        password: Joi.string().min(6).required(),
+        confirmPassword: Joi.string().valid(Joi.ref('password')).required()
+
+    });
+    validateRequest(req,next, schema);
+}
+
+function updateSchema(req, res, next){
+    const schema = Joi.object({
+        title: Joi.string().empty(''),
+        firstName: Joi.string().empty(''),
+        lastName: Joi.string().empty(''),
+        role: Joi.string().valid(Role.Admin, Role.User).empty(''),
+        email: Joi.string().email().empty(''),
+        password: Joi.string().min(6).empty(''),
+        confirmPassword: Joi.string().valid(Joi.ref('password')).empty('')
+
+    }).with('password','confirmPassword');
+    validateRequest(req,next,schema);
+}
